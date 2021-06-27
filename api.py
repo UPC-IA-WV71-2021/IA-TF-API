38a66bc409b971b9e0fa25d5a7eb59e4f324566f
Moises Molina <w7moises@gmail.com>
2021-06-27 00:28:33 -0500

    cnn prediction added

--- api.py
+++ api.py
@@ -4,6 +4,12 @@ from flask.globals import request
 from math import dist
 import csv
 
+import pandas as pd
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import numpy as np
+from sklearn.model_selection import train_test_split
+
 app = Flask(__name__)
 CORS(app)
 
@@ -125,6 +131,62 @@ def ReadDataSet():
             }
             datasets.append(newDataset)
     return jsonify({"message": "Dataset Done"})
+
+@app.route('/cnn', methods = ['POST'])
+@cross_origin()
+def addPersonaCnn():
+
+    df = pd.read_csv("https://raw.githubusercontent.com/UPC-IA-WV71-2021/IA_TF/main/Cleaned-Data.csv")
+    df.head()
+    cols_to_drop = ['None_Sympton', 'Age_0-9', 'Age_10-19', 'Age_20-24', 'Age_25-59', 'Age_60+', 'Gender_Female',
+                    'Gender_Male', 'Gender_Transgender', 'Severity_Mild', 'Severity_Moderate', 'Severity_None',
+                    'Severity_Severe', 'Contact_Dont-Know', 'Contact_No', 'Contact_Yes', 'Country']
+    df = df.drop(cols_to_drop, axis=1)
+    df = df.iloc[310000:,]
+    dataset = df.values
+    arr = np.array((request.json['fever'],
+            request.json['tiredness'],
+            request.json['dry-cough'],
+            request.json['difficulty-in-breathing'],
+            request.json['sore-throat'],
+            request.json['pains'],
+            request.json['nasal-congestion'],
+            request.json['runny-nose'],
+            request.json['diarrhea'],
+            0))
+    dataset2=np.vstack((dataset,arr))
+    X = dataset2[:,0:8]
+    Y = dataset2[:,9]
+    X_train, X_val_test, Y_train, Y_val_test = train_test_split(X, Y, test_size=0.3)
+    X_val, X_test, Y_val, Y_test = train_test_split(X_val_test, Y_val_test, test_size=0.5)
+    model = Sequential([
+        Dense(32, activation='relu', input_shape=(8,)),
+        Dense(32, activation='relu'),
+        Dense(1, activation='sigmoid'),
+    ])
+    model.compile(optimizer='sgd',
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+    h = model.fit(X_train, Y_train,
+                  batch_size=32, epochs=100,
+                  validation_data=(X_val, Y_val))
+    estado = model.predict(X,verbose = 0).tolist()
+    state = estado[-1]
+    newPersona = {
+        "name":request.json['name'],
+        "fever": request.json['fever'],
+        "tiredness": request.json['tiredness'],
+        "dry-cough": request.json['dry-cough'],
+        "difficulty-in-breathing":request.json['difficulty-in-breathing'],
+        "sore-throat":request.json['sore-throat'],
+        "pains": request.json['pains'],
+        "nasal-congestion": request.json['nasal-congestion'],
+        "runny-nose":request.json['runny-nose'],
+        "diarrhea": request.json['diarrhea'],
+        "estado": state
+    }
+    personas.append(newPersona)
+    return jsonify({"message": "Persona Added Succesfully", "personas": personas})
 
 @app.route('/knn', methods = ['POST'])
 @cross_origin()
